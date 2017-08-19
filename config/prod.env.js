1705a1938c53bbedf87a316a5ed9d8325a1e08d0
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2017-08-19 20:50:53 +0200

    switch to prod api endpoint

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   GMAP_API_KEY: '"AIzaSyCeG8L5crkEuoj5H2upp1lVFW1xmlzCZo4"',
-  API_URL: '"http://localhost:8088"'
+  API_URL: '"https://gmalt.bouzekri.net"'
 }
